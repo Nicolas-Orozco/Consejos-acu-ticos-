a01d1dfe9a6ce0406fc4a80258ff3c5d86ea23c2
Nicolas Orozco Davila <93956388+Nicolas-Orozco@users.noreply.github.com>
2021-12-04 00:56:12 +0000

    ADDED OCEAN, IMPROVED SLIDES ANIMATION

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,10 +1,17 @@
-const line1 = document.querySelector('.line')
-  var carousel = document.getElementById('carousel')
-  var textadvise = document.getElementById('textadvise')
-  let carouselbutton1 = document.getElementById('carouselbutton1')
-    let carouselbutton2 = document.getElementById('carouselbutton2')
-      var sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
-      var sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
+const line1 = document.querySelector('.line');
+var carousel = document.getElementById('carousel');
+var textadvise = document.getElementById('textadvise');
+let carouselbutton1 = document.getElementById('carouselbutton1');
+let carouselbutton2 = document.getElementById('carouselbutton2');
+let sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
+let sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
+window.onload = function(){
+ambient.volume = 0.08;
+ambient.play();
+}
+ambient.addEventListener('ended', () => {
+  ambient.play();
+})
 line1.addEventListener('animationend', () => {
   carousel.classList.remove("ocultar")
   carousel.classList.add("enterpostit")
@@ -15,13 +22,13 @@ carousel.addEventListener('animationend', () => {
   textadvise.classList.add("textup")
 })
 textadvise.addEventListener('animationend', () => {
-textadvise.classList.remove("textup")
+  textadvise.classList.remove("textup")
 })
 carouselbutton1.addEventListener('click', () => {
-    sound1.volume = 0.27;
+  sound1.volume = 0.29;
   sound1.play();
 })
 carouselbutton2.addEventListener('click', () => {
-      sound2.volume = 0.27;
+  sound2.volume = 0.29;
   sound2.play();
 })
