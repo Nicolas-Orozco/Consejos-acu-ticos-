7da2e56072e6095afe4b44a086d327f4d1a8d459
Nicolas Orozco Davila <93956388+Nicolas-Orozco@users.noreply.github.com>
2021-12-05 23:08:55 +0000

    ADDED AMBIENT SOUND BUTTON

--- scripts/script2.js
+++ scripts/script2.js
@@ -5,6 +5,9 @@ let carouselbutton1 = document.getElementById('carouselbutton1');
 let carouselbutton2 = document.getElementById('carouselbutton2');
 let sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
 let sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
+let ambtn = document.getElementById('ambientbtn');
+var ambindex = 0;
+   var ambientsound = new Audio('https://drive.google.com/uc?export=download&id=1-ukKbNBy-OoCKQIZ_KjtRnHISpaurOsq')
 line1.addEventListener('animationend', () => {
   carousel.classList.remove("ocultar")
   carousel.classList.add("enterpostit")
@@ -25,3 +28,25 @@ carouselbutton2.addEventListener('click', () => {
   sound2.volume = 0.29;
   sound2.play();
 })
+ambtn.addEventListener('click',() => {
+  ambindex++;
+    for (let numero = 0; numero < 2; numero++) {
+  switch(ambindex){
+    case 0:
+        ambientsound.pause();
+    ambtn.style.backgroundColor = 'rgb(255, 45, 45)';
+    break;
+    case 1:
+    ambientsound.volume = 0.22;
+    ambientsound.play();
+    ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
+    break;
+    case 2:
+    ambindex = 0;
+    break;
+  }
+}})
+ambientsound.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
