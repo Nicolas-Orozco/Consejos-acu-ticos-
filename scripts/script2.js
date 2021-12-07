fc37911e94e80bebcee9220a25cb5ba453eb4eaa
Nicolas Orozco Davila <93956388+Nicolas-Orozco@users.noreply.github.com>
2021-12-07 03:04:00 +0000

    Added the final version of four post its and a guide arrow for the ambient icon, improved organization in script 2

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,17 +1,10 @@
 const line1 = document.querySelector('.line');
-var carousel = document.getElementById('carousel');
-var textadvise = document.getElementById('textadvise');
-let carouselbutton1 = document.getElementById('carouselbutton1');
-let carouselbutton2 = document.getElementById('carouselbutton2');
-let sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
-let sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
-let ambtn = document.getElementById('ambientbtn');
-var ambindex = 0;
-   var ambientsound = new Audio('https://drive.google.com/uc?export=download&id=1-ukKbNBy-OoCKQIZ_KjtRnHISpaurOsq')
 line1.addEventListener('animationend', () => {
   carousel.classList.remove("ocultar")
   carousel.classList.add("enterpostit")
 });
+var carousel = document.getElementById('carousel');
+var textadvise = document.getElementById('textadvise');
 carousel.addEventListener('animationend', () => {
   carousel.classList.remove("enterpostit")
   textadvise.classList.remove("ocultar")
@@ -20,14 +13,21 @@ carousel.addEventListener('animationend', () => {
 textadvise.addEventListener('animationend', () => {
   textadvise.classList.remove("textup")
 })
+let carouselbutton1 = document.getElementById('carouselbutton1');
+let sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
 carouselbutton1.addEventListener('click', () => {
   sound1.volume = 0.29;
   sound1.play();
 })
+let carouselbutton2 = document.getElementById('carouselbutton2');
+let sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
 carouselbutton2.addEventListener('click', () => {
   sound2.volume = 0.29;
   sound2.play();
 })
+let ambtn = document.getElementById('ambientbtn');
+var ambindex = 0;
+var ambientsound = new Audio('https://drive.google.com/uc?export=download&id=1-ukKbNBy-OoCKQIZ_KjtRnHISpaurOsq')
 ambtn.addEventListener('click',() => {
   ambindex++;
     for (let numero = 0; numero < 2; numero++) {
@@ -37,9 +37,11 @@ ambtn.addEventListener('click',() => {
     ambtn.style.backgroundColor = 'rgb(255, 45, 45)';
     break;
     case 1:
-    ambientsound.volume = 0.22;
+    ambientsound.volume = 0.26;
     ambientsound.play();
     ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
+    ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
+    document.getElementById('arrow').style.animation="arrowfade 0.25s linear forwards";
     break;
     case 2:
     ambindex = 0;
