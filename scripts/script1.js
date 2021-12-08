fd4cd362a4e769328fb0a1b781572eb1e4cf605f
Nicolas Orozco Davila <93956388+Nicolas-Orozco@users.noreply.github.com>
2021-12-08 18:05:48 +0000

    Beautifyed css, html and css, addded 2 post its, and media queries to see textadvise

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,55 +2,48 @@
 var count = 1;
 var video1 = document.getElementById("video1");
 var playPromise = video1.play()
+
 function changevideo() {
-  count++;
-  for (let numero = 0; numero < 2; numero++) {
-    switch (count) {
-      case 1:
-        video1.src = "https://drive.google.com/uc?export=download&id=16ieR8-KC9ooK5-QGEIUTH53at45h5tAe";
+    count++;
+    for (let numero = 0; numero < 2; numero++) {
+        switch (count) {
+            case 1:
+                video1.src = "https://drive.google.com/uc?export=download&id=16ieR8-KC9ooK5-QGEIUTH53at45h5tAe";
                 video1.poster = "images/posters/poster4.jpg";
-        if (playPromise !== undefined) {
-          playPromise.then(_ => {
-          })
-            .catch(error => {
-            });
-        }
-        break;
-      case 2:
-        video1.src = "https://drive.google.com/uc?export=download&id=1eRSO5cFHzBu7zILbAwa6FdnqDwL5iN6s";
+                if (playPromise !== undefined) {
+                    playPromise.then(_ => {})
+                        .catch(error => {});
+                }
+                break;
+            case 2:
+                video1.src = "https://drive.google.com/uc?export=download&id=1eRSO5cFHzBu7zILbAwa6FdnqDwL5iN6s";
                 video1.poster = "images/posters/poster3.jpg";
-        if (playPromise !== undefined) {
-          playPromise.then(_ => {
-          })
-            .catch(error => {
-            });
-        }
-        break;
-      case 3:
-        video1.src = "https://drive.google.com/uc?export=download&id=11lQcd0MB6NoPMr3TIkwYgrTcrTqaRCqg";
+                if (playPromise !== undefined) {
+                    playPromise.then(_ => {})
+                        .catch(error => {});
+                }
+                break;
+            case 3:
+                video1.src = "https://drive.google.com/uc?export=download&id=11lQcd0MB6NoPMr3TIkwYgrTcrTqaRCqg";
                 video1.poster = "images/posters/poster2.jpg";
-        if (playPromise !== undefined) {
-          playPromise.then(_ => {
-          })
-            .catch(error => {
-            });
-        }
-        break;
-      case 4:
-        video1.src = "https://drive.google.com/uc?export=download&id=1R4zDjVc5l4BEtc0wVlHTD0ZHLN7OxK6x";
+                if (playPromise !== undefined) {
+                    playPromise.then(_ => {})
+                        .catch(error => {});
+                }
+                break;
+            case 4:
+                video1.src = "https://drive.google.com/uc?export=download&id=1R4zDjVc5l4BEtc0wVlHTD0ZHLN7OxK6x";
                 video1.poster = "images/posters/poster1.jpg";
-        if (playPromise !== undefined) {
-          playPromise.then(_ => {
-          })
-            .catch(error => {
-            });
+                if (playPromise !== undefined) {
+                    playPromise.then(_ => {})
+                        .catch(error => {});
+                }
+                break;
+            case 5:
+                count = 1;
+                break;
         }
-        break;
-      case 5:
-        count = 1;
-        break;
     }
-  }
 }
 /*
 document.getElementById("boton").addEventListener("click",function(){
@@ -62,28 +55,28 @@ document.getElementById("boton").addEventListener("click",function(){
 
 /*DISPLAY THE ANIMATION, HIDE THE BUTTON, AND REDIRECT TO THE POST IT PAGE WHEN CLICKED */
 const $ = document.querySelector.bind(document);
-$('button').addEventListener("click", function () {
-  $('button').style.display = 'none';
-  $('section').style.display = 'block';
-  (document.getElementById("main")).remove();
-  window.location.href = "postit.html";
+$('button').addEventListener("click", function() {
+    $('button').style.display = 'none';
+    $('section').style.display = 'block';
+    (document.getElementById("main")).remove();
+    window.location.href = "postit.html";
 });
 /*LOADING ANIMATION AND FADE */
-window.onload = function () {
-  var fadeTarget = document.getElementById("spinner");
-  var fadeEffect = setInterval(function () {
-    if (!fadeTarget.style.opacity) {
-      fadeTarget.style.opacity = 0.5;
-    }
-    if (fadeTarget.style.opacity > 0) {
-      fadeTarget.style.opacity -= 0.15;
-    } else {
-      clearInterval(fadeEffect);
-    }
-    if (fadeTarget.style.opacity <= 0) {
-      fadeTarget.remove();
-    }
-  }, 150);
-  var main = document.getElementById("main")
-  main.classList.remove("hide");
+window.onload = function() {
+    var fadeTarget = document.getElementById("spinner");
+    var fadeEffect = setInterval(function() {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 0.5;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.15;
+        } else {
+            clearInterval(fadeEffect);
+        }
+        if (fadeTarget.style.opacity <= 0) {
+            fadeTarget.remove();
+        }
+    }, 150);
+    var main = document.getElementById("main")
+    main.classList.remove("hide");
 }
--- scripts/script2.js
+++ scripts/script2.js
@@ -1,59 +1,60 @@
 /*TRIGGER POST IT ANIMATION WHEN LINE ENDS */
 const line1 = document.querySelector('.line');
 line1.addEventListener('animationend', () => {
-  carousel.classList.remove("hide")
-  carousel.classList.add("enterpostit")
+    carousel.classList.remove("hide")
+    carousel.classList.add("enterpostit")
 });
 /*REMOVE ANIMATION CLASSES, AND APPEAR TEXTADVISE WHEN CAROUSEL ANIMATIONS ENDS*/
 var carousel = document.getElementById('carousel');
 var textadvise = document.getElementById('textadvise');
 carousel.addEventListener('animationend', () => {
-  carousel.classList.remove("enterpostit")
-  textadvise.classList.remove("hide")
-  textadvise.classList.add("textup")
+    carousel.classList.remove("enterpostit")
+    textadvise.classList.remove("hide")
+    textadvise.classList.add("textup")
 })
 /*REMOVE ANIMATION STYLES*/
 textadvise.addEventListener('animationend', () => {
-  textadvise.classList.remove("textup")
+    textadvise.classList.remove("textup")
 })
 /*PLAY SOUND1 WHEN BUTTON1 OF CAROUSEL IS CLICKED*/
 let carouselbutton1 = document.getElementById('carouselbutton1');
 let sound1 = new Audio('https://drive.google.com/uc?export=download&id=1tvHrCBnzKByMSxPkVJ2ei5yR6vZwnA5q');
 carouselbutton1.addEventListener('click', () => {
-  sound1.volume = 0.40;
-  sound1.play();
+    sound1.volume = 0.40;
+    sound1.play();
 })
 /*PLAY SOUND2 WHEN BUTTON2 OF CAROUSEL IS CLICKED*/
 let carouselbutton2 = document.getElementById('carouselbutton2');
 let sound2 = new Audio('https://drive.google.com/uc?export=download&id=18cxn99E5PHdOH7AtlvQK1dIdONDp5LOG');
 carouselbutton2.addEventListener('click', () => {
-  sound2.volume = 0.40;
-  sound2.play();
+    sound2.volume = 0.40;
+    sound2.play();
 })
 /*SWITCH WITH INDEX, AMBIENT BUTTTON, AND PLAY THE SOUND*/
 let ambtn = document.getElementById('ambientbtn');
 var ambindex = 0;
 var ambientsound = new Audio('https://drive.google.com/uc?export=download&id=1-ukKbNBy-OoCKQIZ_KjtRnHISpaurOsq')
-ambtn.addEventListener('click',() => {
-  ambindex++;
+ambtn.addEventListener('click', () => {
+    ambindex++;
     for (let numero = 0; numero < 2; numero++) {
-  switch(ambindex){
-    case 0:
-        ambientsound.pause();
-    ambtn.style.backgroundColor = 'rgb(255, 45, 45)';
-    break;
-    case 1:
-    ambientsound.volume = 0.37;
-    ambientsound.play();
-    ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
-    ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
-    document.getElementById('arrow').style.animation="arrowfade 0.25s linear forwards";
-    break;
-    case 2:
-    ambindex = 0;
-    break;
-  }
-}})
+        switch (ambindex) {
+            case 0:
+                ambientsound.pause();
+                ambtn.style.backgroundColor = 'rgb(255, 45, 45)';
+                break;
+            case 1:
+                ambientsound.volume = 0.37;
+                ambientsound.play();
+                ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
+                ambtn.style.backgroundColor = 'rgb(99, 248, 106)';
+                document.getElementById('arrow').style.animation = "arrowfade 0.25s linear forwards";
+                break;
+            case 2:
+                ambindex = 0;
+                break;
+        }
+    }
+})
 /*LOOP FOR THE AMBIENT SOUND*/
 ambientsound.addEventListener('ended', function() {
     this.currentTime = 0;
